f0f8a34e91513e89142348d82cd5775a7c6460af
chara <charakuri8@gmail.com>
2023-07-22 19:48:17 +0900

    20230722 commit

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -3,6 +3,7 @@ import 'package:hirome_rental_owner_web/common/functions.dart';
 import 'package:hirome_rental_owner_web/common/style.dart';
 import 'package:hirome_rental_owner_web/providers/auth.dart';
 import 'package:hirome_rental_owner_web/screens/home.dart';
+import 'package:hirome_rental_owner_web/widgets/animation_background.dart';
 import 'package:hirome_rental_owner_web/widgets/custom_big_button.dart';
 import 'package:hirome_rental_owner_web/widgets/custom_text_box.dart';
 import 'package:hirome_rental_owner_web/widgets/login_title.dart';
@@ -21,68 +22,73 @@ class _LoginScreenState extends State<LoginScreen> {
     final authProvider = Provider.of<AuthProvider>(context);
 
     return ScaffoldPage(
-      content: Center(
-        child: SizedBox(
-          width: 400,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const LoginTitle(),
-              const SizedBox(height: 40),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    children: [
-                      InfoLabel(
-                        label: 'ログインID',
-                        child: CustomTextBox(
-                          controller: authProvider.loginId,
-                          placeholder: '',
-                          keyboardType: TextInputType.text,
-                          maxLines: 1,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      InfoLabel(
-                        label: 'パスワード',
-                        child: CustomTextBox(
-                          controller: authProvider.password,
-                          placeholder: '',
-                          keyboardType: TextInputType.visiblePassword,
-                          maxLines: 1,
-                          obscureText: true,
-                        ),
-                      ),
-                      const SizedBox(height: 24),
-                      CustomBigButton(
-                        labelText: 'ログイン',
-                        labelColor: kWhiteColor,
-                        backgroundColor: kBlueColor,
-                        onPressed: () async {
-                          String? error = await authProvider.signIn();
-                          if (error != null) {
-                            if (!mounted) return;
-                            showMessage(context, error, false);
-                            return;
-                          }
-                          authProvider.clearController();
-                          if (!mounted) return;
-                          Navigator.pushReplacement(
-                            context,
-                            FluentPageRoute(
-                              builder: (context) => const HomeScreen(),
+      content: Stack(
+        children: [
+          const AnimationBackground(),
+          Center(
+            child: SizedBox(
+              width: 400,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const LoginTitle(),
+                  const SizedBox(height: 40),
+                  Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        children: [
+                          InfoLabel(
+                            label: 'ログインID',
+                            child: CustomTextBox(
+                              controller: authProvider.loginId,
+                              placeholder: '',
+                              keyboardType: TextInputType.text,
+                              maxLines: 1,
                             ),
-                          );
-                        },
+                          ),
+                          const SizedBox(height: 8),
+                          InfoLabel(
+                            label: 'パスワード',
+                            child: CustomTextBox(
+                              controller: authProvider.password,
+                              placeholder: '',
+                              keyboardType: TextInputType.visiblePassword,
+                              maxLines: 1,
+                              obscureText: true,
+                            ),
+                          ),
+                          const SizedBox(height: 24),
+                          CustomBigButton(
+                            labelText: 'ログイン',
+                            labelColor: kWhiteColor,
+                            backgroundColor: kBlueColor,
+                            onPressed: () async {
+                              String? error = await authProvider.signIn();
+                              if (error != null) {
+                                if (!mounted) return;
+                                showMessage(context, error, false);
+                                return;
+                              }
+                              authProvider.clearController();
+                              if (!mounted) return;
+                              Navigator.pushReplacement(
+                                context,
+                                FluentPageRoute(
+                                  builder: (context) => const HomeScreen(),
+                                ),
+                              );
+                            },
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-              )
-            ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
