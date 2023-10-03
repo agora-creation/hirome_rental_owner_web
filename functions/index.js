4865086e5a38f164ba60ad4a693c4480dac882da
Shimamura Yuta <shimamura@agora-c.com>
2023-10-03 15:51:41 +0900

    20231003 commit

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,9 @@ const {logger} = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
+const querystring = require('querystring');
+const https = require('https');
+
 //exports.onceYearFunction = onSchedule("00 0 1 1 *", async (event) => {
 exports.onceYearFunction = onSchedule("00 0 * * *", async (event) => {
     let result = await backupToXserver();
@@ -20,7 +23,46 @@ async function backupToXserver() {
     let searchEnd = new Date(dt.getFullYear(), 12, 31, 23, 59, 59);
     let orderQuerySnapshot = await admin.firestore().collection("order").where("createdAt", ">", searchStart).where("createdAt", "<", searchEnd).get();
     orderQuerySnapshot.forEach((orderDoc) => {
-        console.log(orderDoc.data());
+        let orderData = orderDoc.data();
+        //Xserverに設置してあるPHPを実行
+        const post_data = querystring.stringify({
+            "mode": "flutter_send",
+            "id": orderData["id"],
+            "shopId": orderData["shopId"],
+            "shopNumber": orderData["shopNumber"],
+            "shopName": orderData["shopName"],
+            "shopInvoiceName": orderData["shopInvoiceName"],
+            "status": orderData["status"],
+            "updatedAt": orderData["updatedAt"],
+            "createdAt": orderData["createdAt"],
+        });
+        const options = {
+            protocol: "https:",
+            host: "hirome.co.jp",
+            path: "/rental/ajax-order.php",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Content-Length": Buffer.byteLength(post_data)
+            },
+            method: "POST",
+        };
+        const req = https.request(options, (res) => {
+            //PHPからの返答
+            let body = "";
+            res.on("data", (chunk) => {
+                body += chunk;
+            });
+            res.on('end', () => {
+                console.log("応答データはありません");
+            });
+        });
+        req.on("error", (e) => {
+            console.error("リクエストエラー: " + e.message);
+        });
+        req.write(post_data);
+        req.end();
+        //最後にDocを削除
+        //orderDoc.ref.delete();
     });
     return result;
 }
