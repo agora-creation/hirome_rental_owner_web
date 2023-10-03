cd8aaaba992999f3a9f9dd864a789d99ad3c2074
Shimamura Yuta <shimamura@agora-c.com>
2023-10-03 15:06:29 +0900

    20231003 commit

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,24 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const {onSchedule} = require("firebase-functions/v2/scheduler");
 const {logger} = require("firebase-functions");
 
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-exports.onceYearFunction = onSchedule("every day 00:00", async (event) => {
+//exports.onceYearFunction = onSchedule("00 0 1 1 *", async (event) => {
+exports.onceYearFunction = onSchedule("00 0 * * *", async (event) => {
     let result = await backupToXserver();
     console.log(result);
 });
 
 async function backupToXserver() {
-    let result = '失敗';
-
+    let result = "失敗";
+    //Firestoreから2年前の注文データを取得
+    let dt = new Date();
+    let searchStart = new Date(dt.getFullYear() - 2, 1, 1, 0, 0, 0);
+    let searchEnd = new Date(dt.getFullYear() - 2, 12, 31, 23, 59, 59);
+    let orderQuerySnapshot = await admin.firestore().collection("order").where("createdAt", ">", searchStart).where("createdAt", "<", searchEnd).get();
+    orderQuerySnapshot.forEach((orderDoc) => {
+        console.log(orderDoc.data());
+    });
     return result;
 }
